00fccd80ac5db7d60674d2f2276f329137b88fb8
OussamaRomdhane <romdhane.o@sfeir.com>
2019-01-17 16:16:47 +0100

    fix package.json

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ const assetsPath = path.join(__dirname, '../app/dist');
 
 app.use(express.static(assetsPath));
 
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
+});
+
+app.get('*', function(req, res) {
+  res.sendfile(path.join(__dirname, '../app/index.html'));
+});
+
 app.listen(process.env.PORT, () => {
   console.info('Server started at port ', process.env.PORT);
   console.info('Assets hosted at ', assetsPath);
