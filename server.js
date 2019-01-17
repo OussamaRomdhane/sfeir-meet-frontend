d7b49e7a9bdd56a2bb07fabcf065f674f221c27f
OussamaRomdhane <romdhane.o@sfeir.com>
2019-01-17 16:56:07 +0100

    fix package.json

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
-// prettier-ignore
-const
-    path    = require('path'),
-    express = require('express'),
-    app     = express();
-
-app.use(express.static(path.join(__dirname, '../app/dist'), { maxAge: 86400000 }));
-
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.send(500, 'Something broke!');
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function(req, res) {
+  return res.send('pong');
 });
-
 app.get('/', function(req, res) {
-  res.sendfile(path.join(__dirname, '../app/dist/index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
-const port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log('Listening on ' + port);
-});
+app.listen(port);
