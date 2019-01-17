cd3a92ccbb4f35fea04ad7dad1c94a7dfe8f77c6
Oussama Romdhane <Oussama.Igasho@gmail.com>
2019-01-17 18:31:39 +0100

    Fix heroku issues (#4)
    * remove unnecessary logs
    
    * fix package.json
    
    * add favicon
    
    * fix circle ci

---
+++ server.js
@@ -0,0 +1,18 @@
+// prettier-ignore
+const
+  express = require('express'),
+  favicon = require('express-favicon'),
+  path    = require('path'),
+  port    = process.env.PORT,
+  app     = express();
+
+app.use(favicon(__dirname + '/favicon.ico'));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.info('Server started on port', port);
+});
