8a42c3a527d822c0be4f7e28273ab959db956a61
OussamaRomdhane <romdhane.o@sfeir.com>
2019-01-17 17:36:18 +0100

    add logs

--- src/components/App/App.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import App from './App.component.js';
-
-test('Render "My React App!"', () => {
-  const testInstance = shallow(<App />);
-  expect(testInstance.find('h1').text()).toEqual('My React App!');
-});
