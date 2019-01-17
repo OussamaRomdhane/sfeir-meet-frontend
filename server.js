5b248d398a4155b6e213d0957830167dd041ef7f
OussamaRomdhane <romdhane.o@sfeir.com>
2019-01-17 16:35:01 +0100

    fix package.json

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 // prettier-ignore
 const
+    path    = require('path'),
     express = require('express'),
     app     = express();
 
-require('dotenv').config();
+app.use(express.logger());
 
-app.use(express.static('dist'));
+app.use(express.compress());
 
-app.listen(process.env.PORT, () => {
-  console.info('Server started at port ', process.env.PORT);
+app.use(express.static(path.join(__dirname, '../app/dist'), { maxAge: 86400000 }));
+
+app.use(express.bodyParser());
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
+});
+
+app.get('/', function(req, res) {
+  res.sendfile(path.join(__dirname, '../app/dist/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log('Listening on ' + port);
 });
