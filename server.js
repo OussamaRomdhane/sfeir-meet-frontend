b6467953baa4b193ac2523327478003ccc17d549
Oussama Romdhane <Oussama.Igasho@gmail.com>
2019-01-17 22:26:21 +0100

    Passez l'MVP en prod (#6)
    * implement MVP [issue #3]
    
    * fix package.json
    
    * Fix heroku issues (#4)
    
    * remove unnecessary logs
    
    * fix package.json
    
    * add favicon
    
    * fix circle ci
    
    * fix title and favicon

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const
   port    = process.env.PORT,
   app     = express();
 
-app.use(favicon(__dirname + '/favicon.ico'));
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(favicon(__dirname + '/dist/favicon.ico'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
