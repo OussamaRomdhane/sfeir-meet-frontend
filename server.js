29d5469369a4d999edfd40d85a8159b350df5a9d
OussamaRomdhane <romdhane.o@sfeir.com>
2019-01-17 16:28:30 +0100

    fix package.json

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 // prettier-ignore
 const
-    path    = require('path'),
     express = require('express'),
     app     = express();
 
 require('dotenv').config();
 
-const assetsPath = path.join(__dirname, '../app/dist');
-
-app.use('/', express.static(assetsPath));
-
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.send(500, 'Something broke!');
-});
+app.use(express.static('dist'));
 
 app.listen(process.env.PORT, () => {
   console.info('Server started at port ', process.env.PORT);
