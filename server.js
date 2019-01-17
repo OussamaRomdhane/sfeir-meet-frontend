d394e247b73fe03157d3fae092ab439b2cedaa06
OussamaRomdhane <romdhane.o@sfeir.com>
2019-01-17 17:59:41 +0100

    add favicon

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
-app.use(favicon(__dirname + '/dist/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
+// prettier-ignore
+const
+  express = require('express'),
+  favicon = require('express-favicon'),
+  path    = require('path'),
+  port    = process.env.PORT,
+  app     = express();
+
+app.use(favicon(__dirname + '/favicon.ico'));
 app.use(express.static(path.join(__dirname, 'dist')));
-app.get('/ping', function(req, res) {
-  return res.send('pong');
-});
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-app.listen(port);
 
-const dirTree = require('directory-tree');
-console.log(JSON.stringify(dirTree('dist')));
+app.listen(port, () => {
+  console.info('Server started on port', port);
+});
